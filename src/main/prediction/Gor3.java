9af94e9aaafec121f0bc46f080895b70b31a99e0
Mark <sijver@gmail.com>
2013-03-24 19:28:06 +0100

    Gor 3 well done! With missed aminoacids filling algorithm

--- src/main/prediction/Gor3.java
+++ src/main/prediction/Gor3.java
@@ -19,33 +19,50 @@ public class Gor3 {
         this.chouFasman = chouFasman;
     }
 
-    public Protein makePrediction(Protein protein){
+    public Protein makePrediction(Protein protein) {
         double helixScore;
         double betaScore;
         double coilScore;
-        int windowSize = 8;
 
         List<AminoAcid> proteinSequence = protein.getProteinSequence();
 
         List<Structure> predictedProteinStructure = new LinkedList<Structure>();
 
-        for(int i = 0; i < proteinSequence.size(); i++){
+        for (int i = 0; i < proteinSequence.size(); i++) {
             //Self information
-            helixScore = chouFasman.getSelfInformation(proteinSequence.get(i), Structure.HELIX);
-            betaScore = chouFasman.getSelfInformation(proteinSequence.get(i), Structure.BETA);
-            coilScore = chouFasman.getSelfInformation(proteinSequence.get(i), Structure.COIL);
+            helixScore = 0;
+            betaScore = 0;
+            coilScore = 0;
+            if (proteinSequence.get(i) != null) {
+                helixScore = chouFasman.getSelfInformation(proteinSequence.get(i), Structure.HELIX);
+                betaScore = chouFasman.getSelfInformation(proteinSequence.get(i), Structure.BETA);
+                coilScore = chouFasman.getSelfInformation(proteinSequence.get(i), Structure.COIL);
 
-            //Pair information
-            for(int j = i - windowSize; j <= i + windowSize; j++){
-                if(j >= 0 && j < proteinSequence.size() && proteinSequence.get(j) != null && j != i){
-                    helixScore += chouFasman.getPairInformation(proteinSequence.get(i), proteinSequence.get(j), Structure.HELIX);
-                    betaScore += chouFasman.getPairInformation(proteinSequence.get(i), proteinSequence.get(j), Structure.BETA);
-                    coilScore += chouFasman.getPairInformation(proteinSequence.get(i), proteinSequence.get(j), Structure.COIL);
+
+                //Pair information
+                for (int j = i - ChouFasman.WINDOW_SIZE; j <= i + ChouFasman.WINDOW_SIZE; j++) {
+                    if (j >= 0 && j < proteinSequence.size() && proteinSequence.get(j) != null && j != i) {
+                        helixScore += chouFasman.getPairInformation(proteinSequence.get(i), proteinSequence.get(j), Structure.HELIX, j - i);
+                        betaScore += chouFasman.getPairInformation(proteinSequence.get(i), proteinSequence.get(j), Structure.BETA, j - i);
+                        coilScore += chouFasman.getPairInformation(proteinSequence.get(i), proteinSequence.get(j), Structure.COIL, j - i);
+                    }
                 }
+            } else {
+                for (int j = i - ChouFasman.WINDOW_SIZE; j <= i + ChouFasman.WINDOW_SIZE; j++) {
+                    if (j >= 0 && j < proteinSequence.size() && proteinSequence.get(j) != null && j != i) {
+                        for(AminoAcid aminoAcid : AminoAcid.values()){
+                            helixScore += chouFasman.getPairInformation(aminoAcid, proteinSequence.get(j), Structure.HELIX, j - i);
+                            betaScore += chouFasman.getPairInformation(aminoAcid, proteinSequence.get(j), Structure.BETA, j - i);
+                            coilScore += chouFasman.getPairInformation(aminoAcid, proteinSequence.get(j), Structure.COIL, j - i);
+                        }
+                    }
+                }
+                System.out.println(helixScore+ "  " + betaScore + "  " + coilScore);
             }
-            if(helixScore >= betaScore && helixScore >= coilScore){
+
+            if (helixScore >= betaScore && helixScore >= coilScore) {
                 predictedProteinStructure.add(Structure.HELIX);
-            } else if(betaScore >= helixScore && betaScore >= coilScore){
+            } else if (betaScore >= helixScore && betaScore >= coilScore) {
                 predictedProteinStructure.add(Structure.BETA);
             } else {
                 predictedProteinStructure.add(Structure.COIL);
