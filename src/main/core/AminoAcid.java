a224044780289e8248e075ccdff5a75f8dddccbb
mark <sijver@gmail.com>
2013-05-07 20:26:41 +0200

    amino acid letter vs abbreviation has been added

--- src/main/core/AminoAcid.java
+++ src/main/core/AminoAcid.java
@@ -23,5 +23,29 @@ public enum AminoAcid {
     THR,
     TRP,
     TYR,
-    VAL
+    VAL;
+
+    public static AminoAcid getAminoAcidFromLetter(String letter) {
+        if (letter.equalsIgnoreCase("a")) return ALA;
+        else if (letter.equalsIgnoreCase("r")) return ARG;
+        else if (letter.equalsIgnoreCase("n")) return ASN;
+        else if (letter.equalsIgnoreCase("d")) return ASP;
+        else if (letter.equalsIgnoreCase("c")) return CYS;
+        else if (letter.equalsIgnoreCase("q")) return GLN;
+        else if (letter.equalsIgnoreCase("e")) return GLU;
+        else if (letter.equalsIgnoreCase("g")) return GLY;
+        else if (letter.equalsIgnoreCase("h")) return HIS;
+        else if (letter.equalsIgnoreCase("i")) return ILE;
+        else if (letter.equalsIgnoreCase("l")) return LEU;
+        else if (letter.equalsIgnoreCase("k")) return LYS;
+        else if (letter.equalsIgnoreCase("m")) return MET;
+        else if (letter.equalsIgnoreCase("f")) return PHE;
+        else if (letter.equalsIgnoreCase("p")) return PRO;
+        else if (letter.equalsIgnoreCase("s")) return SER;
+        else if (letter.equalsIgnoreCase("t")) return THR;
+        else if (letter.equalsIgnoreCase("w")) return TRP;
+        else if (letter.equalsIgnoreCase("y")) return TYR;
+        else if (letter.equalsIgnoreCase("v")) return VAL;
+        else return null;
+    }
 }
