5fa62ae3dcb95980797b017ba8cfa48ead72c69b
mark <sijver@gmail.com>
2013-05-09 20:44:02 +0200

    MCC has been changed

--- src/main/estimation/MatthewsCorrelationCoefficient.java
+++ src/main/estimation/MatthewsCorrelationCoefficient.java
@@ -32,7 +32,7 @@ public class MatthewsCorrelationCoefficient {
         }
 
         if (truePositive + falsePositive == 0 || truePositive + falseNegative == 0 || trueNegative + falsePositive == 0 || trueNegative + falseNegative == 0) {
-            return -1;
+            return 0;
         } else {
             return ((double) truePositive * trueNegative + falsePositive * falseNegative) / Math.sqrt((truePositive + falsePositive) * (truePositive + falseNegative) * (trueNegative + falsePositive) * (trueNegative + falseNegative));
         }
